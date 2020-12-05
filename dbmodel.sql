69b9cd3f7a269234bcb3bc6f3e5046be284f22ad
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-12-05 17:51:15 +0100

    Update DB model + move back players table + add player colors to have 8 different.

--- dbmodel.sql
+++ dbmodel.sql
@@ -33,3 +33,32 @@
 -- Example 2: add a custom field to the standard "player" table
 -- ALTER TABLE `player` ADD `player_my_custom_field` INT UNSIGNED NOT NULL DEFAULT '0';
 
+-- Table to store cards
+CREATE TABLE IF NOT EXISTS `card` (
+  `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `card_type` varchar(16) NOT NULL,
+  `card_type_arg` int(11) NOT NULL,
+  `card_location` varchar(16) NOT NULL,
+  `card_location_arg` int(11) NOT NULL,
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- Add fields for the number of tokens of each type in player's stock and in the betting area
+ALTER TABLE `player` ADD `player_stock_token_white` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_stock_token_blue` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_stock_token_red` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_stock_token_green` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_stock_token_black` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_bet_token_white` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_bet_token_blue` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_bet_token_red` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_bet_token_green` INT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `player_bet_token_black` INT UNSIGNED NOT NULL DEFAULT '0';
+
+-- Table to store tokens bet on previous betting rounds
+CREATE TABLE IF NOT EXISTS `token` (
+  `token_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `token_color` varchar(16) NOT NULL,
+  `token_number` INT UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`token_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
